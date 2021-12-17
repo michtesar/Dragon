cfa251d5937dc9675e8a3c1a4ac19d842d2865fa
Michael Tesař <michtesar@gmail.com>
2021-12-17 13:50:08 +0100

    Add event docs

--- src/events.h
+++ src/events.h
@@ -3,11 +3,23 @@
 
 #include <SFML/Graphics.hpp>
 
+/**
+ * @brief `sf::Event` processor.
+ * 
+ * Process the the main events from the
+ * control loop.
+ */
 class EventProcessor {
     public:
         void process(sf::Event* event, sf::RenderWindow* window);
 };
 
+/**
+ * @brief Process the event loop.
+ * 
+ * @param event event to process.
+ * @param window window that is affected by event.
+ */
 void EventProcessor::process(sf::Event* event, sf::RenderWindow* window) {
     if (event->type == sf::Event::Closed) {
         window->close();
