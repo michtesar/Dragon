8c7157731e1d2d4c74294b3ffd10cf2f29b3a026
Michael Tesař <michtesar@gmail.com>
2021-12-17 13:56:39 +0100

    Add fractal class docs

--- src/fractal.cpp
+++ src/fractal.cpp
@@ -1,6 +1,17 @@
 #include "fractal.h"
 #include <SFML/Graphics.hpp>
 
+/**
+ * @brief Construct a new Fractal:: Fractal object
+ * 
+ * @param gm graphics manager to perform 
+ * drawing in the buffer.
+ * @param maxIteration number of maximum iteration. It 
+ * is recommended to use maximum 20 to prevent deep recursion. 
+ * Never enter values lesser than zero.
+ * @param offsideX origin for X axis.
+ * @param offsideY origin for Y axis.
+ */
 Fractal::Fractal(
     GraphicsManager* gm, 
     int maxIteration, 
@@ -13,6 +24,22 @@ Fractal::Fractal(
     this->offsideY = offsideY;
 };
 
+/**
+ * @brief Unfold and visualize the dragon curve
+ * at the given iteration.
+ * 
+ * @param iteration number of iteration.
+ * @param x1 line start X coordinate.
+ * @param y1 line start Y coordinate.
+ * @param x2 line end X coordinate.
+ * @param y2 line end Y coordinate.
+ * @param compressionX compression in X axis. Less than
+ * 1.0f opens the fractal. Values more than 1.0f unfolds
+ * the curve more to it's length.
+ * @param compressionY compression in Y axis. Less than
+ * 1.0f opens the fractal. Values more than 1.0f unfolds
+ * the curve more to it's length.
+ */
 void Fractal::unfoldIteration(
     int iteration, 
     float x1, 
